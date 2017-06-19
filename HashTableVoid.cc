b6b54d25b4129e7f490a54acd6fbe9d3f9699be0
Jackie Wu <jackielwu@gmail.com>
2017-06-19 13:28:30 -0400

    3 of 100

--- HashTableVoid.cc
+++ HashTableVoid.cc
@@ -8,22 +8,40 @@
 // Obtain the hash code of a key
 int HashTableVoid::hash(const char * key)
 {
-  // Add implementation here
-  return 0;
+  int sum = 0;
+	int len = strlen(key);
+	for(int i = 0; i < len; i++) {
+		sum += (i+1)*key[i];
+	}
+  return sum % TableSize;
 }
 
 // Constructor for hash table. Initializes hash table
 HashTableVoid::HashTableVoid()
 {
-  // Add implementation here
-       
+  for (int i = 0; i < TableSize; i++) {
+		_buckets[i] = NULL;
+	}    
 }
 
 // Add a record to the hash table. Returns true if key already exists.
 // Substitute content if key already exists.
 bool HashTableVoid::insertItem( const char * key, void * data)
 {
-  // Add implementation here
+  int h = hash(key);
+  HashTableVoidEntry *e = _buckets[h];
+  while (e != NULL) {
+    if(strcmp(key,e->_key)==0) {
+	  	e->_data = data;
+	  	return true;
+		}
+		e = e->_next;
+	}
+	e = new HashTableVoidEntry();
+	e->_key = strdup(key);
+	e->_data = data;
+	e->_next = _buckets[h];
+	_buckets[h] = e;
   return false;
 }
 
